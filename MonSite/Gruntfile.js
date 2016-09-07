0a3020366591678d19d165b1fe65f90bd38cfb63
Romain Bohdanowicz <romain.bohdanowicz@gmail.com>
2016-09-07 12:48:50 +0200

    Fin matinee 3

--- MonSite/Gruntfile.js
+++ MonSite/Gruntfile.js
@@ -1,10 +1,14 @@
 module.exports = function(grunt) {
 
+    grunt.loadNpmTasks('grunt-autoprefixer');
+    grunt.loadNpmTasks('grunt-babel');
     grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-less');
     grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-usemin');
 
     grunt.initConfig({
@@ -13,6 +17,34 @@ module.exports = function(grunt) {
         },
         usemin: {
             dist: 'dist/index.html'
+        },
+        watch: {
+            less: {
+                files: ['src/less/**/*.less'],
+                tasks: ['less:dev', 'autoprefixer:dev']
+            },
+        },
+        autoprefixer: {
+            options: {
+                browsers: ['last 2 versions', 'ie 8', 'ie 9']
+            },
+            dev: {
+                expand: true,
+                cwd: 'src/css',
+                src: '*',
+                dest: 'src/css',
+            }
+        },
+        less: {
+            dev: {
+                expand: true,
+                cwd: 'src/less',
+                src: '*',
+                dest: 'src/css',
+                rename: function (dest, src) {          // The `dest` and `src` values can be passed into the function
+                    return dest + '/' + src.replace('less','css'); // The `src` is being renamed; the `dest` remains the same
+                }
+            }
         },
         uglify: {
             dist: {
